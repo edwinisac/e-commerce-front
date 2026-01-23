d6770c82c7eddb398e07233cb86d90519a290627
edwinisac <edwinisackanjirathinkal@gmail.com>
2026-01-23 17:41:18 +0530

    navbar (polish needed)

--- src/utils/Navbar.jsx
+++ src/utils/Navbar.jsx
@@ -1,13 +1,39 @@
+import { useState } from "react";
 import "./navbar.css";
-import { CiShoppingCart } from "react-icons/ci";
+import { CiShoppingCart, CiHeart, CiUser } from "react-icons/ci";
+import { LuSearch } from "react-icons/lu";
 
 export function Navbar() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
   return (
+
+    
     <div className="nav">
       <img src="logo.svg" alt="logo" />
-      <i className="navicons">
-        <CiShoppingCart/>
-      </i>
+      <div className="search">
+        <button className="searchicon">
+          <LuSearch />
+        </button>
+        <input type="text" placeholder="search for products" />
+      </div>
+      <div className="navicons">
+        {isLoggedIn ? (
+          <>
+            <button>
+              <CiShoppingCart />
+            </button>
+            <button>
+              <CiHeart />
+            </button>
+            <button>
+              <CiUser />
+            </button>
+          </>
+        ) : (
+          <button className="button">Sign In</button>
+        )}
+      </div>
     </div>
   );
 }
